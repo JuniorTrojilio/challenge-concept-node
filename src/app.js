770a910577af7f5165582ce62e3aa8b15ecfda89
Junior Trojilio <junior@Juniors-MacBook-Pro.local>
2020-05-06 10:36:31 -0300

    Challenge Solved

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body
+  const repositorie = {
+    id : uuid(),
+    title,
+    url,
+    techs,
+    likes : 0
+  }
+
+  repositories.push(repositorie)
+  return response.json(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs, likes } = request.body
+
+  const repositorieIndex = repositories.findIndex(r => r.id === id)
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({"Message" : "Error, not find repositorie with this Id"})
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes : repositories[repositorieIndex].likes
+  }
+
+  repositories[repositorieIndex] = repositorie
+
+  return response.json(repositories[repositorieIndex])
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  
+  const repositorieIndex = repositories.findIndex(r => r.id === id)
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({"Message" : "Error, not find repositorie with this Id"})
+  }
+
+  repositories.splice(repositorieIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositorieIndex = repositories.findIndex(r => r.id === id)
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({"Message" : "Error, not find repositorie with this Id, like not applied"})
+  }
+
+  repositories[repositorieIndex].likes += 1
+
+  return response.json(repositories[repositorieIndex])
 });
 
 module.exports = app;
